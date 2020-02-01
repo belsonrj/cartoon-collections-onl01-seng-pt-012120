0a19694542204238c7fbefe741b73de0b9f5ec62
Rick Belson <belsonrj@aol.com>
2020-02-01 20:19:04 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,8 +6,10 @@ def roll_call_dwarves(array)
   end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)
+  i = 0 
+  camptain_planet = [] 
+  
 end
 
 def long_planeteer_calls# code an argument here
