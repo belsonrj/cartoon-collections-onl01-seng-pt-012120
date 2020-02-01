4675461cb817f66d93a4b041c96df0c954dd3fe5
Rick Belson <belsonrj@aol.com>
2020-02-01 20:21:07 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,7 +10,7 @@ def summon_captain_planet(array)
   camptain_planet = [] 
   i = 0 
   while i < array.length 
-    camptain_planet << array[i] 
+    camptain_planet << array[i].capitalize + "!"
 end
 
 def long_planeteer_calls# code an argument here
